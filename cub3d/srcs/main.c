b5565a191fcd545e897eb7b261083c0ac5ed2d79
42corozco <corozco@e2r11p22.42.fr>
2020-02-12 23:16:40 +0100

    norme

--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -6,46 +6,13 @@
 /*   By: corozco <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 22:29:43 by corozco           #+#    #+#             */
-/*   Updated: 2020/02/11 23:29:22 by corozco          ###   ########.fr       */
+/*   Updated: 2020/02/12 23:16:23 by corozco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdlib.h>
 #include <string.h>
-
-/*
-void			print_tex(t_data *d)
-{
-	int			i;
-	int			j;
-
-	i = -1;
-//	printf("\n\npos X [%f], por Y [%f]\n\n\n", d->pl.posx, d->pl.posy);
-	ft_printf("(parametros del parcing)\n");
-	ft_printf("display ->x = %d, ->y = %d\n", d->res.r_x, d->res.r_y);
-//	printf("display ->x = %f, ->y = %f\n", d->pl.planex, d->pl.planey);
-	ft_printf("-no =%s\n", d->tex.north);
-	ft_printf("-so =%s\n", d->tex.south);
-	ft_printf("-we =%s\n", d->tex.west);
-	ft_printf("-ea =%s\n", d->tex.east);
-	ft_printf("-sp =%s\n", d->tex.sprite);
-	ft_printf("-fl =%s\n", d->tex.floor);
-	ft_printf("-mo =%s\n", d->tex.sky);
-	ft_printf("Numero de sprites %d\n", d->sprcount);
-	//while (++i < d->sprcount)
-		//printf("%f - %f\n", d->sprite[i].x, d->sprite[i].y);
-	i = -1;
-	ft_printf("x del mapa = %d y del mapa = %d\n", d->map.x, d->map.y);//borrar
-	while (++i < d->map.y)
-	{
-		j = -1;
-		while (++j < d->map.x)
-			ft_printf("[%d]", d->map.map[i][j]);
-		ft_printf("\n");
-	}
-}
-*/
 
 int				error_management(int ac, char **av)
 {
@@ -69,7 +36,6 @@ int				main(int ac, char **av)
 		ft_ms_error(data.tmps.i_tmp, (void *)0, (void *)0);
 	ft_init_map(av[1], &data);
 	init(&data);
-//	print_tex(&data); //borrar
 	mlx_loop_hook(data.mlx_ptr, raycasting, &data);
 	ft_press(&data);
 	mlx_loop(data.mlx_ptr);
